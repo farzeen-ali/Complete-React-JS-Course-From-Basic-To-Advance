bbeca87ecef6b9b0db2357151bf11c03847e0b61
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-02 21:40:54 +0500

    Add files via upload

---
+++ javascriptRefresher.js
@@ -0,0 +1,79 @@
+// Variable Concepts
+// var a = 10;
+// var a = "Farzeen";
+// console.log(a);
+
+// let b = 8;
+// // let b = "ali"; --> error
+// console.log(b)
+
+// const c = 4;
+// c = 5; //error
+// console.log(c)
+
+//Data Types
+    // Numbers 
+    // Objects
+    // Booleans
+    // Strings
+    // undefined 
+    // null
+
+//Functions
+// function sayHello(){
+//     console.log("Hello");
+// }
+
+// sayHello();
+
+//Objects
+// let emp = {
+//     name : 'farzeen',
+//     age : 23,
+//     func : function myFunc(number){
+//         // console.log("The Number is: " + number);
+//         console.log(`The Number is: ${number}`); //Template Literals
+//     }
+// }
+
+// console.log(emp.name);
+// console.log(emp.age);
+// console.log(emp.func(15));
+
+//Event Listeners
+// document.addEventListener('click', function(){
+//     // alert("Clicked");
+//     confirm("Clicked");
+// })
+
+//use strict mode
+// "use strict";
+// a = 10;
+// console.log(a)
+
+//Arrow Function
+// function num(number){
+//     return number
+// }
+
+// let num = number => number;
+
+// console.log(num(3));
+
+//Array Destructuring
+let arr = ['farzeen',23,true];
+
+let[name, age, yes] = arr;
+
+console.log(name);
+console.log(age);
+console.log(yes);
+
+
+
+
+
+
+
+
+
