575080811df68a19f83db891493de631b659c794
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-03 21:35:36 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import FuncComponent from './FuncComponent';
+// import {FuncComponent} from './FuncComponent';
+function App() {
+  return (
+      <div className='App'>
+        <h1>This is my App.js File</h1>
+        <FuncComponent />
+        {/* <FuncComponent />
+        <FuncComponent />
+        <FuncComponent />
+        <FuncComponent />
+        <FuncComponent /> */}
+      </div>
+  );
+  // function FuncComponent(){
+  //   // export function FuncComponent(){
+  //       return(
+  //       <div>
+  //           <h1>This is my First Function Based Component</h1>
+  //           <h2>Second Heading</h2>
+  //           <h3>Third Heading</h3>
+  //       </div>
+  //       )
+  //   }  
+}
+
+export default App;
---
+++ FuncComponent.js
@@ -0,0 +1,12 @@
+// export function FuncComponent(){
+function FuncComponent(){
+    return(
+    <div>
+        <h1>This is my First Function Based Component</h1>
+        <h2>Second Heading</h2>
+        <h3>Third Heading</h3>
+    </div>
+    )
+}
+
+export default FuncComponent;
