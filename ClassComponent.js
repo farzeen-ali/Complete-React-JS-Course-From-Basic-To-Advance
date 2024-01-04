ce0dba971cf3136c8712baff7358ab26786fc6b2
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-04 21:06:16 +0500

    Add files via upload

---
+++ ClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component{
+    render(){
+        return(
+            <h1>This is my First Class Based Component</h1>
+        )
+    }
+}
+
+export default ClassComponent;
