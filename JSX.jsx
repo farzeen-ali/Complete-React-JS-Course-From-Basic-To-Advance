94ed2101ab7acd6554d25557641d11fecb442c42
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-05 21:23:25 +0500

    Add files via upload

---
+++ JSX.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+function JSX(){
+    return (
+        <h2>JavScript and XML (10+10) {10 + 10}</h2>
+    )
+    //React without JSX
+    // return React.createElement('h2',null,"React Without JSX");
+}
+export default JSX;
