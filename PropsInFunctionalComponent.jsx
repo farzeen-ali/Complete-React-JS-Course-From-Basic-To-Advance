00b684c6cd8b3cb57446ec4993ff30b1e418fb39
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-10 22:18:46 +0500

    Add files via upload

---
+++ PropsInFunctionalComponent.jsx
@@ -0,0 +1,10 @@
+function PropsInFunctionalComponent(props){
+    return(
+        <div>
+            <h2>Hello {props.name}</h2>
+            {/* <h3>Email: {props.email}</h3>
+            <h4>Salary: {props.info.salary} Mobile: {props.info.mobile}</h4> */}
+        </div>
+    )
+}
+export default PropsInFunctionalComponent;
