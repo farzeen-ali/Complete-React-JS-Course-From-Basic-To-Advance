7c8694e340df4c6d4d56aac0b088558a38e2a07a
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-17 22:22:48 +0500

    Add files via upload

---
+++ ReactFragment.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ReactFragment() {
+  return (
+    <React.Fragment>
+    {/* <> */}
+      <h2>React Fragment!!!</h2>
+      <p>Rabia, Saeed, Mazher</p>
+    {/* </> */}
+    </React.Fragment>
+  )
+}
+export default ReactFragment
